4bc7088dad6d9e836f84830db7ede71b8c811f25
Patrick Davlin <patrick@davlin.io>
2019-12-02 17:31:39 -0600

    2

---
+++ index.js
@@ -0,0 +1,24 @@
+const input = require('fs')
+  .readFileSync('../input.txt')
+  .toString('utf-8');
+const getInput = () => input.split(',').map(Number);
+const run = (noun, verb) => {
+  const ints = getInput();
+  ints[1] = noun;
+  ints[2] = verb;
+  let pos = 0;
+  while (ints[pos] !== 99) {
+    const [op, num1Pos, num2Pos, outputPos] = ints.slice(pos, pos+4);
+    const num1 = ints[num1Pos];
+    const num2 = ints[num2Pos];
+    ints[outputPos] = op === 1 ? num1 + num2 : num1 * num2;
+    pos += 4;
+  }
+  return ints[0];
+}
+for (let noun = 0; noun <= 100; noun++) {
+  for (let verb = 0; verb <= 100; verb++) {
+    const localResult = run(noun, verb);
+    if (localResult === 19690720) console.log(100 * noun + verb)
+  }
+}
